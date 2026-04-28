03d2701fe328df225b9875ac2c2c356502e15166
CatDep <tixon.gulbinov@gmail.com>
2026-04-28 23:31:17 +1000

    Добавил FindTreasure

---
+++ 2Month/FindTreasure/FindTreasure.cpp
@@ -0,0 +1,240 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+
+
+bool Treasure(std::vector<std::vector<char>>& map, int &rowTreasure, int &colTreasure)
+{
+    int size = map.size();
+    for (int i = 0; i < size; i++)
+    {
+        int length = map[i].size();
+        for (int j = 0; j < length; j++)
+        {
+            if (map[i][j] == 'T')
+            {
+                rowTreasure = i;
+                colTreasure = j;
+                map[i][j] = '.';
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+void PrintMap(const std::vector <std::vector<char>>& map)
+{
+    int size = map.size();
+    for (int i = 0; i < size; i++)
+    {
+        int length = map[i].size();
+        for (int j = 0; j < length; j++)
+        {
+            std::cout << map[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+
+bool IsInside(const std::vector<std::vector<char>>& map, int row, int col)
+{
+    int size = map.size();
+    if (row >= 0 && row < size)
+    {
+        int length = map[row].size();
+        if (col >= 0 && col < length)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return false;
+}
+
+
+bool IsWall(const std::vector<std::vector<char>>& map, int row, int col)
+{
+    if (not IsInside(map, row, col))
+    {
+        std::cout << "\nЭто стена/ ход невозможен" << std::endl;
+        return true;
+    }
+    if (map[row][col] == '#')
+    {
+        std::cout << "\nЭто стена" << std::endl;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+bool FindPlayer(const std::vector<std::vector<char>>& map, int& rowPlayer, int& colPlayer)
+{
+    int size = map.size();
+    for (int i = 0; i < size; i++)
+    {
+        int length = map[i].size();
+        for (int j = 0; j < length; j++)
+        {
+            if (map[i][j] == 'P')
+            {
+                rowPlayer = i;
+                colPlayer = j;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+bool CanMove(const std::vector<std::vector<char>>& map, int row, int col)
+{
+    if (IsInside(map, row, col) == false)
+    {
+        return false;
+    }
+    if (IsWall(map, row, col) == true)
+    {
+        return false;
+    }
+    return true;
+}
+
+bool IsTreasure(const std::vector<std::vector<char>>& map, int row, int col)
+{
+    int size = map.size();
+    if (IsInside(map, row, col) == true)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            int length = map[i].size();
+            for (int j = 0; j < length; j++)
+            {
+                if (map[row][col] == map[i][j] == 'P')
+                {
+                    return true;
+                }
+            }
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+bool MovePlayer(std::vector<std::vector<char>>& map, char command, bool& isWin)
+{
+    int row = 0;
+    int col = 0;
+
+    if (FindPlayer(map, row, col) == true)
+    {
+        int newRow = row;
+        int newCol = col;
+        if (command == 'w')
+        {
+            newRow--;
+        }
+        else if (command == 's')
+        {
+            newRow++;
+        }
+        else if (command == 'a')
+        {
+            newCol--;
+        }
+        else if (command == 'd')
+        {
+            newCol++;
+        }
+        else
+        {
+            std::cout << "Ход невозможен" << std::endl;
+            return false;
+
+        }
+
+        if (CanMove(map, newRow, newCol) == false)
+        {
+            std::cout << "Ход невозможен" << std::endl;
+            return false;
+        }
+
+
+
+        map[row][col] = '.';
+        map[newRow][newCol] = 'P';
+        return true;
+    }
+    return false;
+}
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+
+    std::vector<std::vector<char>> map =
+    {
+        {'#','#','#','#','#'},
+        {'#','P','.','.','#'},
+        {'#','.','.','.','#'},
+        {'#','.','.','T','#'},
+        {'#','#','#','#','#'}
+    };
+    bool isPlay = true;
+    bool isWin = false;
+    char command = ' ';
+    int row = 0;
+    int col = 0;
+
+    bool hasTreasure = Treasure(map, row, col);
+
+    if (hasTreasure == false)
+    {
+        std::cout << "Сокровище не найдено" << std::endl; 
+        return 0;
+    }
+
+    while (isPlay == true)
+    {
+        PrintMap(map);
+        std::cout << "Введите куда вы хотите походить: ";
+        std::cin >> command;
+        if (command == 'q')
+        {
+            isPlay = false;
+            break;
+        }
+        MovePlayer(map, command, isWin);
+        
+        int playerRow = 0;
+        int playerCol = 0;
+
+        FindPlayer(map, playerRow, playerCol);
+
+
+        if (playerRow == row && playerCol == col)
+        {
+            std::cout << "Вы нашли сокровище" << std::endl;
+            isPlay = false;
+            break;
+        }
+        
+
+    }
+}
